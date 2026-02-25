c03b143d79605b2e66bf1fda33db89da1bcdf6c4
Luiz Felipe Ribeiro Souza <lipezerafar@gmail.com>
2026-02-25 09:37:12 -0300

    feat: implementa toString em ProdutoPerecivel

--- src/ProdutoNaoPerecivel.java
+++ src/ProdutoNaoPerecivel.java
@@ -1,2 +1,15 @@
-public class ProdutoNaoPerecivel {
+public class ProdutoNaoPerecivel extends Produto{
+
+    public ProdutoNaoPerecivel(String desc, double precoCusto, double margemLucro) {
+        super(desc, precoCusto, margemLucro);
+    }
+
+    public ProdutoNaoPerecivel(String desc, double precoCusto){
+        super(desc, precoCusto);
+    }
+
+    public double valorVenda(){
+        return (precoCusto * (1.0 + margemLucro));
+    }
+
 }
--- src/ProdutoPerecivel.java
+++ src/ProdutoPerecivel.java
@@ -1,2 +1,42 @@
-public class ProdutoPerecivel {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class ProdutoPerecivel extends Produto {
+
+    private double DESCONTO = 0.25;
+    private int PRAZO_DESCONTO = 7;
+    private LocalDate dataDeValidade;
+
+    public ProdutoPerecivel(String desc, double precoCusto, double margemLucro, LocalDate dataDeValidade, double DESCONTO, int PRAZO_DESCONTO) {
+        super(desc, precoCusto, margemLucro);
+
+        if(dataDeValidade.isBefore(LocalDate.now())){
+            throw new IllegalArgumentException("O produto está vencido!");
+        } else {
+            this.dataDeValidade = dataDeValidade;
+        }
+
+    }
+
+    public double valorVenda() {
+        double desconto = 0d;
+        int tempoDesconto = LocalDate.now().until(dataDeValidade).getDays();
+        if(tempoDesconto<=PRAZO_DESCONTO){
+            desconto = DESCONTO;
+        }
+        return(precoCusto * (1.0 + margemLucro))*(1-desconto);
+    }
+
+    @Override
+    public String toString() {
+
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        String dados = super.toString();
+        dados += "\nValido até " + formato.format(dataDeValidade);
+        return dados;
+
+    }
+
 }
